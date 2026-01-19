902f913a31f2c98c5423d1e396f54d7ba933db9c
mohammed shafin <shafinms21@gmail.com>
2026-01-20 02:29:26 +0530

    rft dmn

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -37,6 +37,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      domain: '.mtechcare.com',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       path: '/',
     });
